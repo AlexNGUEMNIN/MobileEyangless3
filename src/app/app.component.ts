5d4d7e2b244b0ca8e4b40e6c2e3c2983c7dd8f98
AlexNGUEMNIN <alexnguemnin@gmail.com>
2025-05-22 00:47:47 +0100

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { IonApp, IonRouterOutlet, IonHeader } from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  standalone: true,
+  imports: [IonHeader,  IonApp, IonRouterOutlet],
+})
+export class AppComponent {
+  
+  
+  constructor() {
+    
+  }
+}
