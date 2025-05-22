e6540ea1b96ceb50667f26140bb7fd84fbb49ec1
AlexNGUEMNIN <163337134+AlexNGUEMNIN@users.noreply.github.com>
2025-05-23 00:25:46 +0100

    Updated app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -26,14 +26,7 @@ export const routes: Routes = [
         path: 'home',
         loadComponent: () => import('./pages/home/home.page').then(m => m.HomePage)
       },
-      {
-        path: 'profile',
-        loadComponent: () => import('./pages/profile/profile.page').then(m => m.ProfilePage)
-      },
-      {
-        path: 'settings',
-        loadComponent: () => import('./pages/settings/settings.page').then(m => m.SettingsPage)
-      },
+     
       {
         path: '',
         redirectTo: 'home',
